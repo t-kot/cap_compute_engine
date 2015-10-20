cca57e5ca49bf0a2ac464ff2c88a115aa271dfbc
kotohata <kotohata@attracie.com>
2015-10-20 19:43:21 +0900

    Update

--- lib/cap_compute_engine.rb
+++ lib/cap_compute_engine.rb
@@ -1,5 +1,8 @@
 require 'cap_compute_engine/version'
 require 'cap_compute_engine/instance_list'
+require 'json'
+require 'active_support'
+require 'active_support/core_ext'
 
 module CapComputeEngine
 end
